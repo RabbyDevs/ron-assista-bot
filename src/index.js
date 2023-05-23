52093f77694116f8be653d9f206e3897a7620b3f
RabbyDevs <rabby79127@gmail.com>
2023-05-23 23:51:42 +0300

    critical bug fixed, bot wasn't starting.

--- src/index.js
+++ src/index.js
@@ -25,7 +25,8 @@ for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 	for (const file of commandFiles) {
-		const command = require(`./commands/${file}`);
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
 		commands.push(command.data.toJSON());
 	}
 }
