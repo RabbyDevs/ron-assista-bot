9cee63d79e0e1e5f3424a20aab54daaa5f2752d9
RabbyDevs <rabby79127@gmail.com>
2023-11-10 15:45:50 +0300

    revamp command from 1.9 (1.9.1

--- src/commands/embedders/makeEmbed.js
+++ src/commands/embedders/makeEmbed.js
@@ -17,38 +17,85 @@ module.exports = {
 		.addStringOption(option =>
 			option
 				.setName('body')
-				.setDescription('Description or body of embed, separate with "|"')
+				.setDescription('Description or body of embed, separate with "|" input custom to initiate custom formatting.')
 				.setRequired(true))
 		.setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
 	async execute(interaction) {
 		await interaction.deferReply();
 		await interaction.editReply('Making and sending embeds...');
 		const titles = interaction.options.getString('title').split('|');
-		const bodies = interaction.options.getString('body').split('|');
+		let bodies = interaction.options.getString('body').split('|');
         const forum = interaction.options.getString('forum');
+        let done = false
     
 		let bodyNumber = 0;
-		for (const title of titles) {
-            interaction.guild.channels.fetch(forum)
-                .then(channel => {
-                    // inside a command, event listener, etc.
-                    const embed = new EmbedBuilder()
-                    .setColor(0x6E1C09)
-                    .setTitle(title)
-                    .setDescription(bodies[bodyNumber])
-                    .setTimestamp()
-                            
-                    channel.threads.create({
-                        name: title,
-                        autoArchiveDuration: 4320,
-                            message: {
-                                embeds: [embed],
-                            },
-                            reason: title,
-                        })
-                });
-			bodyNumber = (bodyNumber[bodyNumber + 1] ? bodyNumber + 1 : bodyNumber);
-		}
-		await interaction.followUp('Done!');
+        let titleNumber = 0;
+
+        async function forLoop() {
+            return new Promise(async (resolve, reject) =>{
+                for (const title of titles) {
+                    if (bodies[bodyNumber] == 'custom') {
+                        const collectorFilter = m => m.author.id.includes(interaction.user.id);
+                        const collector = interaction.channel.createMessageCollector({ filter: collectorFilter, time: 15000 });
+                        await interaction.followUp(`Input your custom body for "${title}".`);
+        
+                        collector.on('collect', m => {
+                            collector.stop()
+                            interaction.followUp('Input received!')
+                            bodies[bodyNumber] = m.content
+                            interaction.guild.channels.fetch(forum) 
+                            .then(channel => {
+                                // inside a command, event listener, etc.
+                                const embed = new EmbedBuilder()
+                                .setColor(0x6E1C09)
+                                .setTitle(title)
+                                .setDescription(bodies[bodyNumber])
+                                .setTimestamp()
+                                        
+                                channel.threads.create({
+                                    name: title,
+                                    autoArchiveDuration: 4320,
+                                        message: {
+                                            embeds: [embed],
+                                        },
+                                        reason: title,
+                                    })
+                            });
+                            if (titleNumber == titles.length) {
+                                resolve('Done!')
+                            }
+                        });
+                    }
+                    else {
+                        interaction.guild.channels.fetch(forum) 
+                        .then(channel => {
+                            // inside a command, event listener, etc.
+                            const embed = new EmbedBuilder()
+                            .setColor(0x6E1C09)
+                            .setTitle(title)
+                            .setDescription(bodies[bodyNumber])
+                            .setTimestamp()
+                                    
+                            channel.threads.create({
+                                name: title,
+                                autoArchiveDuration: 4320,
+                                    message: {
+                                        embeds: [embed],
+                                    },
+                                    reason: title,
+                                })
+                        });
+                        if (titleNumber == titles.length) {
+                            resolve('Done!')
+                        }
+                    }
+                    bodyNumber = (bodyNumber[bodyNumber + 1] ? bodyNumber + 1 : bodyNumber);
+                    titleNumber = titleNumber + 1
+                }
+            })
+        }
+        forLoop().then(async returned => {
+            await interaction.followUp(returned)
+        })
 	},
 };
